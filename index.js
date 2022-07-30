0a0c473f888927834d4c253bc55007927ff68baf
CalFy <2006nak@gmail.com>
2022-07-30 13:14:40 +0300

    onInstalled bug

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function selection() {
 setTimeout(() => { 
     console.log(selection)
 },10000)
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        title: "Translator", 
+        id: "translator",
+        onclick: handleClick,
+        contexts: ['selection']
+    })    
+});
+
+function handleClick(info) {
+    let l = document.createElement("h1")
+    l.innerText = info.selectionText
+    document.body.append(info.selectionText)
+}
